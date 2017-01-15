6896546a6ea3cab93c88a3524a22b56d7b9d6b96
antony <antony@antony.local>
2017-01-15 09:28:38 +0800

    hello world

---
+++ course-test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  course-test
+//
+//  Created by antony on 2017/1/15.
+//  Copyright © 2017年 antony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("here")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
